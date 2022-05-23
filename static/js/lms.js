4df9ec509e6052bfa2f030765fa731aae0599bdf
PrinandaRahmatullah <prinanda50@gmail.com>
2022-05-23 18:22:27 +0700

    add precision calculation function, store points

--- static/js/lms.js
+++ static/js/lms.js
@@ -76,6 +76,35 @@ window.onload = async function () {
         .begin();
 }
 
+
+// TODO : Calculate Calibration Accuracy : makes the variables true for 5 seconds & plots the points
+//  Nanti bisa diletakkan setelah kalibrasi selesai
+// store_points_variable(); // start storing the prediction points
+
+// delay(5000).then(() => {
+//     stop_storing_points_variable(); // stop storing the prediction points
+
+//     var past50 = webgazer.getStoredPoints(); // retrieve the stored points
+//     var precision_measurement = calculatePrecision(past50);
+//     swal({
+//         title: "Your accuracy measure is " + precision_measurement + "%",
+//         allowOutsideClick: false,
+//         buttons: {
+//             cancel: "Recalibrate",
+//             confirm: true,
+//         }
+//     }).then(isConfirm => {
+//         if (isConfirm) {
+//             //clear the calibration & hide the last middle button
+
+//         } else {
+//             //use restart function to restart the calibration
+//             Restart();
+//         }
+//     });
+// });
+
+
 // Set to true if you want to save the data even if you reload the page.
 window.saveDataAcrossSessions = true;
 
@@ -83,9 +112,99 @@ window.onbeforeunload = function () {
     webgazer.end();
 }
 
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+
 /**
  * Restart the calibration process by clearing the local storage
  */
- function Restart() {
+// TODO : Restart function
+function Restart() {
     webgazer.clearData();
+}
+
+
+/*
+ * Sets store_points to true, so all the occuring prediction
+ * points are stored
+ */
+function store_points_variable(){
+    webgazer.params.storingPoints = true;
+  }
+  
+  /*
+   * Sets store_points to false, so prediction points aren't
+   * stored any more
+   */
+  function stop_storing_points_variable(){
+    webgazer.params.storingPoints = false;
+  }
+  
+
+
+
+/*
+ * This function calculates a measurement for how precise 
+ * the eye tracker currently is which is displayed to the user
+ */
+function calculatePrecision(past50Array) {
+    var windowHeight = screen.height;
+    var windowWidth = screen.width;
+
+    // Retrieve the last 50 gaze prediction points
+    var x50 = past50Array[0];
+    var y50 = past50Array[1];
+
+    // Calculate the position of the point the user is staring at
+    var staringPointX = windowWidth / 2;
+    var staringPointY = windowHeight / 2;
+
+    var precisionPercentages = new Array(50);
+    calculatePrecisionPercentages(precisionPercentages, windowHeight, x50, y50, staringPointX, staringPointY);
+    var precision = calculateAverage(precisionPercentages);
+
+    // Return the precision measurement as a rounded percentage
+    return Math.round(precision);
+};
+
+/*
+ * Calculate percentage accuracy for each prediction based on distance of
+ * the prediction point from the centre point (uses the window height as
+ * lower threshold 0%)
+ */
+function calculatePrecisionPercentages(precisionPercentages, windowHeight, x50, y50, staringPointX, staringPointY) {
+    for (x = 0; x < 50; x++) {
+        // Calculate distance between each prediction and staring point
+        var xDiff = staringPointX - x50[x];
+        var yDiff = staringPointY - y50[x];
+        var distance = Math.sqrt((xDiff * xDiff) + (yDiff * yDiff));
+
+        // Calculate precision percentage
+        var halfWindowHeight = windowHeight / 2;
+        var precision = 0;
+        if (distance <= halfWindowHeight && distance > -1) {
+            precision = 100 - (distance / halfWindowHeight * 100);
+        } else if (distance > halfWindowHeight) {
+            precision = 0;
+        } else if (distance > -1) {
+            precision = 100;
+        }
+
+        // Store the precision
+        precisionPercentages[x] = precision;
+    }
+}
+
+/*
+ * Calculates the average of all precision percentages calculated
+ */
+function calculateAverage(precisionPercentages) {
+    var precision = 0;
+    for (x = 0; x < 50; x++) {
+        precision += precisionPercentages[x];
+    }
+    precision = precision / 50;
+    return precision;
 }
