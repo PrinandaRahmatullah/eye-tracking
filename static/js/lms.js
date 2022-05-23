6d3eceb609e514499e167374e43746ac95386ba2
PrinandaRahmatullah <prinanda50@gmail.com>
2022-05-23 15:19:38 +0700

    add text log and restart function

--- static/js/lms.js
+++ static/js/lms.js
@@ -5,65 +5,87 @@ let focusState = false; // pop indikasi nyontek
 let recordOn = false;
 let saveToBE = false; // saving to backend 
 
-// Webgazer activated
-webgazer
-    .setRegression("ridge")
-    .setTracker("TFFacemesh")
-    .setGazeListener(function (data, elapsedTime) {
-        if (data == null) {
-            console.log("Warning : No Face Detected!");
-            faceDetected = false;
-        } 
-        else {
-            // not focus
-            if (
-                data.x <= 0 ||
-                data.x >= screen.width ||
-                data.y <= 0 ||
-                data.y >= screen.height
-            ) {
-                if (timeCounter === null || !timeCounter) {
-                    timeCounter = new Date();
-                    console.log("Warning : Anda tidak fokus!");
-                    recordOn = true;
-                    
-                }
+var textLog = document.getElementById("log");
 
-                focusState = false;
-            }
-            // back to focus
-            else {
-                if (timeCounter && timeCounter !== null) {
-                    // timer on
-                    let secondsDifference = parseFloat(
-                        (new Date() - timeCounter) / 1000
-                    );
-                    if (secondsDifference > 5) {
-                        console.log(
-                            "Warning : Anda dicuragai menyontek! Compile and Upload recorded data! - " +
-                            secondsDifference +
-                            " seconds"
-                        );
-                        saveToBE = true;
-                        // TODO : compile, upload record
+window.onload = async function () {
+    webgazer.showVideoPreview(false) /* shows all video previews */
+        .showPredictionPoints(true) /* shows a square every 100 milliseconds where current prediction is */
+        .applyKalmanFilter(true);
 
-                    } else {
-                        // Belum cukup syarat
-                        console.log(
-                            "Warning : Delete recorded data! - " +
-                            secondsDifference +
-                            " seconds"
-                        );
+    // Webgazer activated
+    await webgazer
+        .setRegression("ridge")
+        .setTracker("TFFacemesh")
+        .setGazeListener(function (data, elapsedTime) {
+            if (data == null) {
+                // console.log("Warning : No Face Detected!");
+                textLog.innerHTML += "Warning : No Face Detected!<br>";
+                faceDetected = false;
+            } else {
+                // not focus
+                if (
+                    data.x <= 0 ||
+                    data.x >= screen.width ||
+                    data.y <= 0 ||
+                    data.y >= screen.height
+                ) {
+                    if (timeCounter === null || !timeCounter) {
+                        timeCounter = new Date();
+                        textLog.innerHTML += "Warning : Anda tidak fokus!<br>";
+                        // console.log("Warning : Anda tidak fokus!");
+                        recordOn = true;
+
                     }
-                    // TODO : delete record
 
-                    timeCounter = null;
-                    recordOn = false;
+                    focusState = false;
                 }
+                // back to focus
+                else {
+                    if (timeCounter && timeCounter !== null) {
+                        // timer on
+                        let secondsDifference = parseFloat(
+                            (new Date() - timeCounter) / 1000
+                        );
+                        if (secondsDifference > 5) {
+                            // console.log(
+                            //     "Warning : Anda dicuragai menyontek! Compile and Upload recorded data! - " +
+                            //     secondsDifference +
+                            //     " seconds"
+                            // );
+                            textLog.innerHTML += "Warning : Anda dicuragai menyontek! Compile and Upload recorded data! - " + secondsDifference + " seconds<br>";
+                            saveToBE = true;
+                            // TODO : compile, upload record
 
-                focusState = true;
+                        } else {
+                            // Belum cukup syarat
+                            // console.log("Warning : Delete recorded data! - " + secondsDifference + " seconds");
+                            textLog.innerHTML += "Warning : Delete recorded data! - " + secondsDifference + " seconds<br>";
+                        }
+                        // TODO : delete record
+
+                        timeCounter = null;
+                        recordOn = false;
+                    }
+
+                    focusState = true;
+                }
+                faceDetected = true;
             }
-            faceDetected = true;
-        }
-    })
-    .begin();
+        })
+        .saveDataAcrossSessions(true)
+        .begin();
+}
+
+// Set to true if you want to save the data even if you reload the page.
+window.saveDataAcrossSessions = true;
+
+window.onbeforeunload = function () {
+    webgazer.end();
+}
+
+/**
+ * Restart the calibration process by clearing the local storage
+ */
+ function Restart() {
+    webgazer.clearData();
+}
