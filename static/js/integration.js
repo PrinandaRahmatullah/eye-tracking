f892f6b214874933ca7ec1e99d21c3364ac1e563
PrinandaRahmatullah <prinanda50@gmail.com>
2022-10-27 15:09:44 +0700

    try to integrate face-api & webgazer. Still failed

--- static/js/integration.js
+++ static/js/integration.js
@@ -1,73 +1,71 @@
-let SSD_MOBILENETV1 = 'ssd_mobilenetv1'
+// let SSD_MOBILENETV1 = 'ssd_mobilenetv1'
 let TINY_FACE_DETECTOR = 'tiny_face_detector'
 
 
-let selectedFaceDetector = SSD_MOBILENETV1
+let selectedFaceDetector = TINY_FACE_DETECTOR
 
-// ssd_mobilenetv1 options
-let minConfidence = 0.5
+// // ssd_mobilenetv1 options
+// let minConfidence = 0.5
 
 // tiny_face_detector options
-let inputSize = 512
-let scoreThreshold = 0.5
+let inputSize = 256
+let scoreThreshold = 0.45
 
 function getFaceDetectorOptions() {
-    return selectedFaceDetector === SSD_MOBILENETV1
-        ? new faceapi.SsdMobilenetv1Options({ minConfidence })
-        : new faceapi.TinyFaceDetectorOptions({ inputSize, scoreThreshold })
+    return new faceapi.TinyFaceDetectorOptions({ inputSize, scoreThreshold })
 }
 
-function onIncreaseMinConfidence() {
-    minConfidence = Math.min(faceapi.utils.round(minConfidence + 0.1), 1.0)
-    $('#minConfidence').val(minConfidence)
-    updateResults()
-}
+// function onIncreaseMinConfidence() {
+//     minConfidence = Math.min(faceapi.utils.round(minConfidence + 0.1), 1.0)
+//     $('#minConfidence').val(minConfidence)
+//     updateResults()
+// }
 
-function onDecreaseMinConfidence() {
-    minConfidence = Math.max(faceapi.utils.round(minConfidence - 0.1), 0.1)
-    $('#minConfidence').val(minConfidence)
-    updateResults()
-}
+// function onDecreaseMinConfidence() {
+//     minConfidence = Math.max(faceapi.utils.round(minConfidence - 0.1), 0.1)
+//     $('#minConfidence').val(minConfidence)
+//     updateResults()
+// }
 
-function onInputSizeChanged(e) {
-    changeInputSize(e.target.value)
-    updateResults()
-}
+// function onInputSizeChanged(e) {
+//     changeInputSize(e.target.value)
+//     updateResults()
+// }
 
-function changeInputSize(size) {
-    inputSize = parseInt(size)
+// function changeInputSize(size) {
+//     inputSize = parseInt(size)
 
-    let inputSizeSelect = $('#inputSize')
-    inputSizeSelect.val(inputSize)
-    inputSizeSelect.material_select()
-}
+//     let inputSizeSelect = $('#inputSize')
+//     inputSizeSelect.val(inputSize)
+//     inputSizeSelect.material_select()
+// }
 
-function onIncreaseScoreThreshold() {
-    scoreThreshold = Math.min(faceapi.utils.round(scoreThreshold + 0.1), 1.0)
-    $('#scoreThreshold').val(scoreThreshold)
-    updateResults()
-}
+// function onIncreaseScoreThreshold() {
+//     scoreThreshold = Math.min(faceapi.utils.round(scoreThreshold + 0.1), 1.0)
+//     $('#scoreThreshold').val(scoreThreshold)
+//     updateResults()
+// }
 
-function onDecreaseScoreThreshold() {
-    scoreThreshold = Math.max(faceapi.utils.round(scoreThreshold - 0.1), 0.1)
-    $('#scoreThreshold').val(scoreThreshold)
-    updateResults()
-}
+// function onDecreaseScoreThreshold() {
+//     scoreThreshold = Math.max(faceapi.utils.round(scoreThreshold - 0.1), 0.1)
+//     $('#scoreThreshold').val(scoreThreshold)
+//     updateResults()
+// }
 
-function onIncreaseMinFaceSize() {
-    minFaceSize = Math.min(faceapi.utils.round(minFaceSize + 20), 300)
-    $('#minFaceSize').val(minFaceSize)
-}
+// function onIncreaseMinFaceSize() {
+//     minFaceSize = Math.min(faceapi.utils.round(minFaceSize + 20), 300)
+//     $('#minFaceSize').val(minFaceSize)
+// }
 
-function onDecreaseMinFaceSize() {
-    minFaceSize = Math.max(faceapi.utils.round(minFaceSize - 20), 50)
-    $('#minFaceSize').val(minFaceSize)
-}
+// function onDecreaseMinFaceSize() {
+//     minFaceSize = Math.max(faceapi.utils.round(minFaceSize - 20), 50)
+//     $('#minFaceSize').val(minFaceSize)
+// }
 
 function getCurrentFaceDetectionNet() {
-    if (selectedFaceDetector === SSD_MOBILENETV1) {
-        return faceapi.nets.ssdMobilenetv1
-    }
+    // if (selectedFaceDetector === SSD_MOBILENETV1) {
+    //     return faceapi.nets.ssdMobilenetv1
+    // }
     if (selectedFaceDetector === TINY_FACE_DETECTOR) {
         return faceapi.nets.tinyFaceDetector
     }
@@ -95,24 +93,24 @@ async function changeFaceDetector(detector) {
     $('#loader').hide()
 }
 
-async function onSelectedFaceDetectorChanged(e) {
-    selectedFaceDetector = e.target.value
+// async function onSelectedFaceDetectorChanged(e) {
+//     selectedFaceDetector = e.target.value
 
-    await changeFaceDetector(e.target.value)
-    updateResults()
-}
+//     await changeFaceDetector(e.target.value)
+//     updateResults()
+// }
+// 
+// function initFaceDetectionControls() {
+// let faceDetectorSelect = $('#selectFaceDetector')
+// faceDetectorSelect.val(selectedFaceDetector)
+// faceDetectorSelect.on('change', onSelectedFaceDetectorChanged)
+// faceDetectorSelect.material_select()
 
-function initFaceDetectionControls() {
-    let faceDetectorSelect = $('#selectFaceDetector')
-    faceDetectorSelect.val(selectedFaceDetector)
-    faceDetectorSelect.on('change', onSelectedFaceDetectorChanged)
-    faceDetectorSelect.material_select()
-
-    let inputSizeSelect = $('#inputSize')
-    inputSizeSelect.val(inputSize)
-    inputSizeSelect.on('change', onInputSizeChanged)
-    inputSizeSelect.material_select()
-}
+// let inputSizeSelect = $('#inputSize')
+// inputSizeSelect.val(inputSize)
+// inputSizeSelect.on('change', onInputSizeChanged)
+// inputSizeSelect.material_select()
+// }
 
 async function onPlay() {
     let videoEl = $('#inputVideo').get(0)
@@ -127,11 +125,11 @@ async function onPlay() {
     let result = await faceapi.detectAllFaces(videoEl, options)
     console.log(result.length);
 
-    if (result) {
-        let canvas = $('#overlay').get(0)
-        let dims = faceapi.matchDimensions(canvas, videoEl, true)
-        faceapi.draw.drawDetections(canvas, faceapi.resizeResults(result, dims))
-    }
+    // if (result) {
+    //     let canvas = $('#overlay').get(0)
+    //     let dims = faceapi.matchDimensions(canvas, videoEl, true)
+    //     faceapi.draw.drawDetections(canvas, faceapi.resizeResults(result, dims))
+    // }
 
     setTimeout(() => onPlay())
 }
@@ -139,16 +137,17 @@ async function onPlay() {
 async function run() {
     // load face detection model
     await changeFaceDetector(TINY_FACE_DETECTOR)
-    changeInputSize(128)
+    // changeInputSize(256)
 
     // try to access users webcam and stream the images
     // to the video element
     let stream = await navigator.mediaDevices.getUserMedia({ video: {} })
     let videoEl = $('#inputVideo').get(0)
+
     videoEl.srcObject = stream
 }
 
 $(document).ready(function () {
-    initFaceDetectionControls()
+    // initFaceDetectionControls()
     run()
 })
